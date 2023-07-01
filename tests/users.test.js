2dc0b0e208bb6d00196aae4c258c6b45752b8782
jmsmith23 <smitjeff23@gmail.com>
2023-07-01 14:51:47 -0500

    Add login and logout tests and see that they pass

--- tests/users.test.js
+++ tests/users.test.js
@@ -2,7 +2,7 @@ const request = require('supertest');
 const mongoose = require('mongoose');
 const { MongoMemoryServer } = require('mongodb-memory-server');
 const app = require('../app');
-const server = app.listen(8081, () => console.log('testing, testing 1, 2, 3'));
+const server = app.listen(8081, () => console.log('testing, testing 80... 81'));
 const Post = require('../models/Post');
 const User = require('../models/User');
 let mongoServer;
@@ -29,7 +29,41 @@ describe('Test all of the users endpoints', () => {
     expect(response.statusCode).toBe(200);
     expect(response.body.user.name).toEqual('J Smith');
     expect(response.body.user.email).toEqual('jms@hemail.com');
-    expect(response.body).to.have.ownProperty('token');
+    expect(response.body).toHaveProperty('token');
+  });
+
+  test('It should login the user', async () => {
+    const user = new User({
+      name: 'J Smith',
+      email: 'jms@login.com',
+      password: '123456',
+    });
+    await user.save();
+    const response = await request(app)
+      .post('/users/login')
+      .send({ name: 'J Smith', email: 'jms@login.com', password: '123456' });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.user.email).toEqual('jms@login.com');
+    expect(response.body).toHaveProperty('token');
+  });
+
+  test('It should logout the user', async () => {
+    const user = new User({
+      name: 'J Smith',
+      email: 'jms@logout.com',
+      password: '123456',
+    });
+    await user.save();
+    const token = await user.generateAuthToken();
+
+    const response = await request(app)
+      .post(`/users/logout/${user.id}`)
+      .set('Authorization', `Bearer ${token}`)
+      .send({ name: 'J Smith', email: 'jms@logout.com', password: '123456' });
+
+    expect(response.statusCode).toBe(200);
+    expect(response.body.message).toBe('You are successfully logged out');
   });
 
   test('It should update user info', async () => {
