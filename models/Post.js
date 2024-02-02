c4f8db9fa0a0acf405ba4fea6bd9bb2b58109e51
Jeff Smith <127367482+jmsmith23@users.noreply.github.com>
2024-02-01 22:19:50 -0600

    update user schema

--- models/Post.js
+++ models/Post.js
@@ -4,11 +4,10 @@ const postSchema = new Schema(
   {
     title: { type: String, required: true },
     user: { type: Schema.Types.ObjectId, ref: 'User' },
-    category: { type: String, required: true },
-    post: { type: String, required: true },
+    content: { type: String, required: true },
     likeUserIds: [{ type: Schema.Types.ObjectId, ref: 'User' }],
     comments: [{ type: Schema.Types.ObjectId, ref: 'Comment' }],
-    image: { type: String, required: true },
+    thumbnail: { type: String, required: true },
     tags: [{ type: Schema.Types.ObjectId, ref: 'Tag' }],
   },
   {
